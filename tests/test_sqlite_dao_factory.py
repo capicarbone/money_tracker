dff4d99c458819934d3840132664f9167ad18952
Carlos Pinelly <cpinelly@gmail.com>
2023-11-20 12:22:44 -0500

    Formatting

--- tests/test_sqlite_dao_factory.py
+++ tests/test_sqlite_dao_factory.py
@@ -1,5 +1,9 @@
 import unittest
-from money_tracker.daos.sql_generic.dao import GenericSQLAccountDAO, GenericSQLCategoryDAO, GenericSQLTransactionDAO
+from money_tracker.daos.sql_generic.dao import (
+    GenericSQLAccountDAO,
+    GenericSQLCategoryDAO,
+    GenericSQLTransactionDAO,
+)
 from money_tracker.daos.sql_generic import SQLiteDAOFactory
 
 
@@ -11,14 +15,12 @@ class SqliteDAOFactoryTest(unittest.TestCase):
 
         self.assertIsInstance(dao, GenericSQLAccountDAO)
 
-    
     def test_get_category_dao(self):
         factory = SQLiteDAOFactory("test.db")
 
         dao = factory.get_category_dao()
 
         self.assertIsInstance(dao, GenericSQLCategoryDAO)
-
 
     def test_get_category_dao(self):
         factory = SQLiteDAOFactory("test.db")
