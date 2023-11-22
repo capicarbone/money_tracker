aa4ce9724d247cf319b5284bdaafe667df1687a2
Carlos Pinelly <cpinelly@gmail.com>
2023-11-22 16:03:09 -0500

    tables creation  on factory instantiation.

--- money_tracker/daos/sql_generic/factory.py
+++ money_tracker/daos/sql_generic/factory.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Engine, create_engine
+from sqlalchemy import Engine, create_engine, inspect
 from money_tracker.daos.base import (
     AbsAccountsDAO,
     AbsDAOFactory,
@@ -10,6 +10,7 @@ from money_tracker.daos.sql_generic.dao import (
     GenericSQLCategoryDAO,
     GenericSQLTransactionDAO,
 )
+from money_tracker.daos.sql_generic.models import Base
 
 
 class SQLiteDAOFactory(AbsDAOFactory):
@@ -17,6 +18,9 @@ class SQLiteDAOFactory(AbsDAOFactory):
 
     def __init__(self, file_path) -> None:
         self.engine = create_engine(f"sqlite+pysqlite:///{file_path}")
+        if not inspect(self.engine).has_table("account"):
+            Base.metadata.create_all(self.engine)
+
 
     def _create_account_dao(self) -> AbsAccountsDAO:
         return GenericSQLAccountDAO(self.engine)
